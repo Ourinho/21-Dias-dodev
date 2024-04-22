3735ab47921f14fe2107e4aac0032c8211a62e9d
Thiago de Matos <oxumourinho@gmail.com>
2024-04-21 22:10:41 -0300

    dia-3

---
+++ dia 3/index.js
@@ -0,0 +1,23 @@
+    //Declaração de variaveis
+
+let nome = ""
+let idade = 0
+let altura = 0
+let peso = 0
+
+    //Solicitando informação ao usuario e atribuindo os valores as variaveis
+
+nome = prompt("Digite seu nome: ")
+idade = Number(prompt("Digite sua idade: "))
+altura = parseFloat(prompt("Digite sua altura: "))
+peso = Number(prompt("Digite seu peso: "))
+
+    //Calculando o ano que a pessoa nasceu e o IMC
+let soma = 2023 - idade
+ 
+let imc =0
+imc = peso / (altura * altura)
+
+    //Exibindo as informaçpes no console
+
+console.log("Olá " + nome +", voçê tem " + idade + " anos, nasceu em " + soma +", tem " + altura + " de altura, pesa " + peso+"kg seu IMC é " + imc +"Kg/m2" )
