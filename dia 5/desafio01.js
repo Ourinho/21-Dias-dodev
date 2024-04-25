a5bb87cb1195e513460438cd7949f20f3803e6ac
Thiago de Matos <oxumourinho@gmail.com>
2024-04-25 20:48:03 -0300

    dia 5

---
+++ dia 5/desafio01.js
@@ -0,0 +1,19 @@
+let num1 = Number(prompt("Insira seu primeiro numero:"))
+let num2 = Number(prompt("Insira seu segundo numero:"))
+let opcao = Number(prompt("Qual operação que deseja realizar? " + ("\n1 = +; \n2 = -; \n3 = *; \n4 = /; ")))
+
+switch (opcao) {
+    case 1:
+        console.log(num1 + " + " + num2 + " = " + (num1 + num2))
+        break;
+    case 2:
+        console.log(num1 + " - " + num2 + " = " + (num1 - num2))
+        break;
+    case 3:
+        console.log(num1 + " * " + num2 + " = " + (num1 * num2))
+        break;
+    
+    default:
+        console.log(num1 + " / " + num2 + " = " + (num1 / num2))
+        break;
+}
---
+++ dia 5/desafio02.js
@@ -0,0 +1,17 @@
+let posto = prompt("Qual serviço deseja fazer:" + "\n1 = Gasolina \n2 = àlcool \n3 = Calibrar")
+
+switch (posto) {
+    case "1":
+        valor = Number(prompt("Quanto quer de gasolina?"))
+        quantidade = valor / 5
+        console.log("Você abasteceu " + quantidade.toFixed(2) + "L de gasolina")
+        break;
+    case "2":
+        valor = Number(prompt("Quanto quer de àlcool?"))
+        quantidade = valor / 3 
+        console.log("Você abasteceu " + quantidade.toFixed(2) + "L de àlcool")
+        break;
+    default :
+        console.log("Pneus calibrados com sucesso")
+        break;
+}
