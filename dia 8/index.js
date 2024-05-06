d9f98fc3f5afb37f2cd5a07b2162659d7395b491
Thiago de Matos <oxumourinho@gmail.com>
2024-05-06 16:14:27 -0300

    dia 8

---
+++ dia 8/index.js
@@ -0,0 +1,17 @@
+/// FUNÇAO
+
+function somar(num1, num2) {
+    console.log(`Estou dentro da função somar`)
+    console.log(num1)
+    console.log(num2)
+}
+
+somar(1, 2);
+
+/// COM RETURN
+
+function somar(num1, num2) {
+    return num1 + num2;
+}
+
+let resultadoSoma = somar(1, 2)
