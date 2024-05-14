e0cd836501911f1200a59040d0cb3684d9b792e2
Thiago de Matos <oxumourinho@gmail.com>
2024-05-14 01:31:49 -0300

    dia 10

---
+++ dia 10/index.js
@@ -0,0 +1,17 @@
+let nomes = [];
+let continuar = true;
+let posicaoDoArray = 0;
+
+while(continuar) {
+    let nomeInserido = prompt("Insira um nome: ")
+    nomes[posicaoDoArray] = nomeInserido
+
+    let desejaContinuar = prompt("Insira 1 caso deseje adicionar um novo nome")
+    if(desejaContinuar != '1') {
+        continuar = false;
+        continue;
+    }    
+    posicaoDoArray++;
+}
+ 
+console.log(nomes)
