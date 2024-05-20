fc96b7bdfed7322560686296bc833fffdf0de1e3
Thiago de Matos <oxumourinho@gmail.com>
2024-05-19 22:12:57 -0300

    dia 6

--- dia 6/desafio02.js
+++ dia 6/desafio02.js
@@ -1,4 +1,4 @@
-//CRIANDO UM CONTADOR QUE CONTA 5 EM 5
+//CRIANDO UM CONTADOR QUE CONTA 5 EM 5.
 console.log("Contagem 5 em 5")
 for(let contador = 0; contador <= 50; contador += 5) {
     console.log(contador)   
