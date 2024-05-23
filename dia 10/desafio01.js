11d300db3fcc494e474c2cf629ce4f2aa0a5c7a5
Thiago de Matos <oxumourinho@gmail.com>
2024-05-23 18:40:57 -0300

    dia 10

--- dia 10/desafio01.js
+++ dia 10/desafio01.js
@@ -5,12 +5,12 @@ let contadorIndice = 0;
 
     const numeroProcurado = parseInt(prompt("Digite o número a ser procurado: "))
 
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 10; i++) { // Supomos que o array tem até 10 elementos
     const elemento = parseInt(prompt(`Digite o elemento ${i + 1}:`))
     array[i] = elemento;
 }
 
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 10; i++) { // Supomos que o array tem até 10 elementos
     if (array[i] === numeroProcurado) {
         indices[contadorIndice] = i
         contadorIndice++
---
+++ dia 10/desafio02.js
@@ -0,0 +1,20 @@
+//CRIANDO VARIÃVEIS 
+let array = []
+let arrayInvertido = []
+
+//POPULANDO ARRAY
+for (let i = 0; i < 5; i++) {
+    let numero = parseInt(prompt(`Digite o ${i + 1}° número`))
+    array[i] = numero
+}
+
+console.log("Array original: " + array)
+
+//INVERTINDO ARRAY
+let contador = 4
+for (let i = 0; i < 5; i++) {
+    arrayInvertido[i] = array[contador]
+    contador--
+}
+
+console.log("Array invertido: " + arrayInvertido)
