9c9f857c45ad1de8c9814f5715d408ba3baf14c4
Thiago de Matos <oxumourinho@gmail.com>
2024-05-29 23:12:29 -0300

    dia 11

---
+++ dia 11/desafio01.js
@@ -0,0 +1,25 @@
+//CRIANDO VARIAVEIS
+let alunos = []
+let notas = []
+let continuar = 's'
+let contador = 0
+
+//CRIANDO LOOP
+while(continuar === 's' ){
+
+    ArrayAlunos = prompt("Insira seu nome: ")
+    console.log(nome)
+    ArrayNotas = Number(prompt("Insira sua nota: "))
+    console.log(nota)
+
+    alunos[contador] = nome
+    notas[contador] = nota
+    contador++
+    
+    continuar = prompt("deseja continuar inseriando informação dos alunos? (s/n)")
+
+    if (continuar === 'n'){
+    
+    }  
+    
+}
