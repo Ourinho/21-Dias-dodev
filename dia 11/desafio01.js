861ec96b4e61fd8d5280ca8979d290e9e049ec8b
Thiago de Matos <oxumourinho@gmail.com>
2024-05-31 19:58:28 -0300

    dia 11

--- dia 11/desafio01.js
+++ dia 11/desafio01.js
@@ -7,19 +7,31 @@ let contador = 0
 //CRIANDO LOOP
 while(continuar === 's' ){
 
-    ArrayAlunos = prompt("Insira seu nome: ")
+    let nome = prompt("Insira seu nome: ")
     console.log(nome)
-    ArrayNotas = Number(prompt("Insira sua nota: "))
+    let nota = Number(prompt("Insira sua nota: "))
     console.log(nota)
 
     alunos[contador] = nome
     notas[contador] = nota
     contador++
     
-    continuar = prompt("deseja continuar inseriando informação dos alunos? (s/n)")
+    let resposta = prompt("deseja continuar inseriando informação dos alunos? (s/n)")
 
-    if (continuar === 'n'){
-    
-    }  
-    
+    if (resposta == 'n')
+        continuar = false     
 }
+
+//EXIBINDO INFORMAÇÕES
+console.log('Notas dos alunos: ')
+for (let i =0; i < alunos.length; i++) {
+    console.log(`${alunos[i]} = ${notas[i]}`)
+}
+
+let somaNotas = 0
+for (let i =0; i < notas.length; i++) {
+    somaNotas += notas[i]
+}
+let media = somaNotas / alunos.length
+console.log(`A soma das notas foi: ${somaNotas}`)
+console.log(`A média geral da turma foi: ${media}`)
