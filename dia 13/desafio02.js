d35b5fc334d09e92cb4268356791be9c23869d0d
Thiago de Matos <oxumourinho@gmail.com>
2024-06-20 23:36:17 -0300

    dia 13

--- dia 13/desafio02.js
+++ dia 13/desafio02.js
@@ -15,8 +15,47 @@ class Carro{
         return resultado;
     }
 }
-console.log(`Me u carro é uma ${this.nome}, ele tem ${this.potencia} de cavalos, a velocidade maxima dele é de ${this.velocidadeMaxima} ele pega de 0 a 100km em ${this.aceleracao}.`) 
 
-const informacaoDoCarro = new Carro('McLaren', 700, 360, 3);
+class Corrida {
+    nome
+    tipo
+    distacia
+    participantes
+    vencedor
+    constructor(nome, tipo, distacia){
+        this.nome = nome
+        this.tipo = tipo
+        this.distacia = distacia
+        this.participantes = []
+        this.vencedor = ""
+    }
 
-informacaoDoCarro.CalculeTempoMedio();
+    DefinirVencedor(){
+        let menorTempo = this.participantes[0].CalculeTempoMedio(this.distacia)
+        let vencedor = this.participantes[0]
+
+        for(let index = 1; index < this.participantes.length; index++){
+            let tempo = this.participantes[index].CalculeTempoMedio(this.distacia)
+            if (tempo < menorTempo) {
+                menorTempo = tempo
+                vencedor = this.participantes[index]
+            }
+        }
+
+        return this.vencedor = vencedor
+    }
+
+    ExibirVencedor(){
+        alert("O vencedor é; " + this.vencedor.nome)
+    }
+}
+
+let corrida = new Corrida("paris", "formula 1", 60000)
+
+corrida.participantes[0] = new Carro("fusca", 120, 160, 3)
+corrida.participantes[1] = new Carro("Fiat",210, 200, 6)
+corrida.participantes[2] = new Carro("palio", 300, 220, 8)
+
+corrida.DefinirVencedor()
+corrida.ExibirVencedor()
+
